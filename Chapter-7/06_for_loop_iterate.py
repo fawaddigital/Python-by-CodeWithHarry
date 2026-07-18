b89a3fa8dcdea6c9d88729013de2c76db2e7999c
fawad_18 <fawaddigitalx@gmail.com>
2026-07-18 11:21:16 +0500

    Add for_loop iterate

---
+++ Chapter-7/06_for_loop_iterate.py
@@ -0,0 +1,20 @@
+# for Loop Through a List
+l = [1, 5, 9, 44, 60, 99]
+
+# "Take each element from the list l one by one and store it in the variable i.
+for i in l:
+    print(i)
+
+print("End of loop")
+
+
+# for loop through a Tuple
+t = {18, "fawad", False, "ali"}
+for i in t:
+    print(i)
+
+
+# for loop through a Stings
+s = "fawad"
+for i in s:
+    print(i)
