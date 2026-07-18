d4d7686c2f99b4a1c738089e642b39dd74f28762
fawad_18 <fawaddigitalx@gmail.com>
2026-07-18 11:16:24 +0500

    Added 01,02,03 to the Chapter-7

---
+++ Chapter-7/01_loops.py
@@ -0,0 +1,16 @@
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+
+# The same task can be done like this: 
+for i in range(1, 6):
+    print(i)
+
+# A loop is used to execute a block of code repeatedly until a condition is met or for a specific number of times.
+'''
+    Primarily there are two types of loops in python. 
+    • while loops 
+    • for loops 
+'''
---
+++ Chapter-7/02_while_loop.py
@@ -0,0 +1,14 @@
+# A while loop is used to repeatedly execute a block of code as long as a condition is True.
+
+i = 1
+
+# Run the loop while i is less than 6
+while(i < 6):
+    
+    # Print the current value of i
+    print(i)
+    
+    # Increase the value of i by 1
+    i += 1
+
+    # i += 1 increases the value of i by 1 and is equivalent to i = i + 1.
