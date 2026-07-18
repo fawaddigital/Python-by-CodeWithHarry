7b477b1b718d6ff6bc0bd245b883e97d70d35553
fawad_18 <fawaddigitalx@gmail.com>
2026-07-18 11:18:53 +0500

    Added list using while

---
+++ Chapter-7/04_list_using_while.py
@@ -0,0 +1,13 @@
+l = [1, "fawad", "ali", 18, "khan", False, 99.88]
+
+#  Start index from 0
+i = 0
+
+#  Run the loop while i is less than the length of the list
+while(i<len(l)):
+    
+    # Print the element at index i
+    print(l[i])
+
+    # Move to the next index
+    i += 1
