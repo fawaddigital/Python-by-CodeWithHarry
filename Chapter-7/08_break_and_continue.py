ef012d17d44d9ba872957f3eb84bc12f9850d339
fawad_18 <fawaddigitalx@gmail.com>
2026-07-19 16:25:42 +0500

    Add break and continue

---
+++ Chapter-7/08_break_and_continue.py
@@ -0,0 +1,13 @@
+# For loop with break statement.
+for i in range(50):
+    if (i == 20):
+        break   # Exit the loop right now.
+    print(i)
+print("End of a Loop")
+
+
+# For loop with continue statement.
+for i in range(50):
+    if(i == 25):
+        continue  # Skip this iteration. Continue means skip this value..
+    print(i)
