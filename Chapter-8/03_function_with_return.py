4b52144fd1308ca2799081a59d33535a0990e885
fawad_18 <fawaddigitalx@gmail.com>
2026-07-23 11:30:09 +0500

    Added function with return

---
+++ Chapter-8/03_function_with_return.py
@@ -0,0 +1,9 @@
+# Function with return value: A function that sends a result back to the caller using the return statement.
+def add(a, b):
+
+     # Return the sum of a and b to (add) varaible
+    return a + b
+
+# Store the returned value in result (add value moves to result).
+result = add(5, 3)
+print(result)
