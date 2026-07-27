5dc54bc2c53c55b49fd18e384d638f336da4936d
fawad_18 <fawaddigitalx@gmail.com>
2026-07-27 23:08:39 +0500

    Added problem 7

---
+++ Chapter-8-PS/07_problem7.py
@@ -0,0 +1,11 @@
+# 7. Write a python function to remove a given word from a list ad strip it at the same 
+# time. 
+def rem(l, word):
+    n = []
+    for item in l:
+        if not(item == word):
+            n.append(item.strip(word))
+    return n
+
+l = ["fawad", "Rohan", "Rashdan", "an"]
+print(rem(l, "an"))
