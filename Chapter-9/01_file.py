382dcd2a1b1f912f6ad24d8c192529ca2ffe38ab
fawad_18 <fawaddigitalx@gmail.com>
2026-07-28 22:21:21 +0500

    Added files

---
+++ Chapter-9/01_file.py
@@ -0,0 +1,17 @@
+'''
+a = "a very long string with emails"
+
+emails = []
+3 seconds
+
+'''
+# open() is a built-in Python function used to open a file so you can read from it, write to it, or append data to it.
+f = open("file.text")
+
+# Read the entire contents of the file and store them in the variable 'data'
+data = f.read()
+
+print(data)
+
+# Close the file to free up system resources
+f.close()
