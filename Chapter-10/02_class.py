f761618ffe564ad43f7414289ea14e5d9a321861
fawad_18 <fawaddigitalx@gmail.com>
2026-08-10 11:00:54 +0500

    Added class in OOPs

---
+++ Chapter-10/02_class.py
@@ -0,0 +1,15 @@
+ # Here we are creating a class named employee
+class employee:    
+    name = "fawad"
+    language = "python"
+    salary = 100000         # name, language, and salary are attributes of the employee class.
+
+# harry is an object of the employee class.
+fawad = employee()  
+print(fawad.name, fawad.language, fawad.salary)
+
+harry = employee()
+harry.name = "harry"  # Here we are changing the name attribute of the harry object to "harry".
+harry.language = "java" 
+print(harry.name, harry.language, harry.salary) 
+
