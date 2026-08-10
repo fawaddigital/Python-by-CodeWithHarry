4f933bdd3ad4bb7fa1ae95a7f539825084e7f2ea
fawad_18 <fawaddigitalx@gmail.com>
2026-08-10 10:58:59 +0500

    OOP Definitions

---
+++ Chapter-10/01_OOP_def.py
@@ -0,0 +1,44 @@
+'''
+(OOP) is a programming style where you write code by creating classes (blueprints) and objects (real instances).
+It helps you organize related data and functions together, making your code easier to write, understand, and reuse.
+
+Simple Example
+Think about a Car 🚗.
+
+A car has:
+
+Properties (Data):
+Color
+Brand
+Model
+
+A car can perform:
+
+Actions (Methods):
+Start
+Stop
+Accelerate
+
+'''
+'''
+What is Class?
+A class is a blueprint for creating objects. It defines the properties and methods that the objects created from the class will have.
+
+Example:
+class Car:
+
+What is Object?
+An object is an instance of a class. It represents a specific car with its own unique properties and behaviors.
+
+Example:
+    my_car = Car() 
+
+'''
+class Student:
+    pass
+
+student1 = Student()
+student2 = Student()
+
+# Student is the class.
+# student1 and student2 are objects of the Student class.
